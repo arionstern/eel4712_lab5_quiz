64d3fee30ae953036a136c68abad3d28a2d06548
arionstern <aristern06@gmail.com>
2026-02-28 15:38:05 -0500

    added vga.vhd

--- src/part4/vga.vhd
+++ src/part4/vga.vhd
@@ -18,4 +18,46 @@ end vga;
 
 architecture str of vga is
 
+--define signals 
+signal Horiz_Sync, Vert_Sync, Video_On, Col_En, Row_En : std_logic;
+signal Hcount, Vcount       : std_logic_vector(9 downto 0);
+signal pos    :      std_logic_vector(2 downto 0);
+signal Row_Addr, Col_Addr      : std_logic_vector(5 downt 0);
+
+
+begin
+
+       --define components
+       U_vga_sync_gen : entity work.VGA_sync_gen
+        port map (
+            clk        => clk,
+            rst        => rst,
+            Hcount     => Hcount,
+            Vcount     => Vcount,
+            Horiz_Sync => Horiz_Sync,
+            Vert_Sync  => Vert_Sync,
+            Video_On   => Video_On
+        );
+
+
+       U_col_addr_gen : entity work.col_addr_gen
+        port map (
+            Hcount   => Hcount,
+            pos      => pos,
+            Col_Addr => Col_Addr,
+            Col_En   => Col_En
+        );
+
+
+       U_row_addr_gen : entity work.row_addr_gen
+        port map (
+            Vcount   => Vcount,
+            pos      => pos,
+            Row_Addr => Row_Addr,
+            Row_En   => Row_En
+        );
+
+
+        
+
 end str;
